72ee917ec8fb9958901e6cd78bf5ba3996449923
Nova <xplodnstar@live.com>
2019-04-30 11:35:59 -0700

    commit it!

--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -2,33 +2,33 @@ import React, { useEffect } from 'react';
 import { getCurrCat } from '../actions/actions';
 import { connect } from 'react-redux'
 
-// const PostForm = (props) => {
-//     useEffect(() => {
-//         getCurrCat(props.slug)
+const PostForm = (props) => {
+    useEffect(() => {
+        getCurrCat(props.slug)
 
-//     }, [props.slug])
-// }
+    }, [props.slug])
+}
 
-// return (
-//     <div>
-//         <h1>{props.name}</h1>
-//         <div className="listSearch"></div>
-//         <div className="listNav"></div>
-//         <ul>
-//             {props.list.map(cat => (
-//                 <li key={`'child ' + ${cat.id}`}><Link to={`/${cat.slug}`}>{cat.name}</Link></li>
-//             ))}
-//         </ul>
-//     </div>
-// )
+return (
+    <div>
+        <h1>{props.name}</h1>
+        <div className="listSearch"></div>
+        <div className="listNav"></div>
+        <ul>
+            {props.list.map(cat => (
+                <li key={`'child ' + ${cat.id}`}><Link to={`/${cat.slug}`}>{cat.name}</Link></li>
+            ))}
+        </ul>
+    </div>
+)
 
 
-// function mapStateToProps(appState, ownProps) {
-//     console.log('here', appState)
-//     return {
-//         name: appState.currCat.name,
-//         slug: ownProps.match.params.slug
-//     }
-// }
+function mapStateToProps(appState, ownProps) {
+    console.log('here', appState)
+    return {
+        name: appState.currCat.name,
+        slug: ownProps.match.params.slug
+    }
+}
 
 export default connect(mapStateToProps)(PostForm)
